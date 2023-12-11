26fd30715afd3d5a380501e2f884311d0f0edf2d
Mahesh <maheshmhaske241198@gmail.com>
2023-12-11 15:35:42 +0530

    ok

--- react/src/components/HTTP/Api.jsx
+++ react/src/components/HTTP/Api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 const baseURL = "http://localhost:3001";
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  // baseURL: "http://localhost:3001",
+  baseURL: "http://leadplanner.lotusx.shop/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application.json",
